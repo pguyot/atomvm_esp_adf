137ea65aa8acf6f685bbd5f9d0e0d357bd503278
Paul Guyot <pguyot@kallisys.net>
2024-05-07 18:19:31 +0200

    Add mp3 example
    Also add channels and bits to i2s output configuration.
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

---
+++ examples/play_mp3_i2s/README.md
@@ -0,0 +1,20 @@
+play_mp3_i2s
+=====
+
+Sample code to plan an MP3 sound on an I2S card.
+
+It was tested with an esp32c3 card connected to a MAX98357A module.
+
+The MAX98357A module is connected to the following esp32c3 gpios:
+
+- LRCLK: gpio 7
+- BCLK: gpio 6
+- DIN: gpio 5
+
+The MAX98357A module doesn't require any MCLK, so this example doesn't
+configure a gpio for it.
+
+Build and flash
+---------------
+
+    $ rebar3 atomvm esp32_flash -p /dev/cu.usbmodem*
---
+++ examples/play_mp3_i2s/src/play_mp3_i2s.erl
@@ -0,0 +1,50 @@
+%%%-------------------------------------------------------------------
+%% @doc Example code to play an MP3 file over i2s
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(play_mp3_i2s).
+
+-export([start/0]).
+
+%% GPIO configuration
+
+-define(I2S_LRC_GPIO, 7).
+-define(I2S_BCLK_GPIO, 6).
+-define(I2S_DIN_GPIO, 5).
+
+start() ->
+    io:format("Play MP3 using AtomVM ESP ADF\n"),
+
+    AudioPipeline = esp_adf_audio_pipeline:init([]),
+
+    MP3File = atomvm:read_priv(?MODULE, "adf_music.mp3"),
+
+    io:format("MP3File: ~B bytes\n", [byte_size(MP3File)]),
+
+    MP3Decoder = esp_adf_mp3_decoder:init([]),
+    ok = esp_adf_audio_element:set_read_binary(MP3Decoder, MP3File),
+    ok = esp_adf_audio_pipeline:register(AudioPipeline, MP3Decoder, <<"mp3">>),
+
+    I2SOutput = esp_adf_i2s_output:init([
+        {rate, 44100},
+        {bits, 16},
+        {gpio_bclk, ?I2S_BCLK_GPIO},
+        {gpio_lrclk, ?I2S_LRC_GPIO},
+        {gpio_dout, ?I2S_DIN_GPIO}
+    ]),
+    ok = esp_adf_audio_pipeline:register(AudioPipeline, I2SOutput, <<"i2s">>),
+
+    ok = esp_adf_audio_pipeline:link(AudioPipeline, [<<"mp3">>, <<"i2s">>]),
+
+    ok = esp_adf_audio_pipeline:run(AudioPipeline),
+
+    timer:sleep(7000),
+
+    ok = esp_adf_audio_pipeline:stop(AudioPipeline),
+    ok = esp_adf_audio_pipeline:wait_for_stop(AudioPipeline),
+    ok = esp_adf_audio_pipeline:terminate(AudioPipeline),
+    ok = esp_adf_audio_pipeline:unregister(AudioPipeline, MP3Decoder),
+    ok = esp_adf_audio_pipeline:unregister(AudioPipeline, I2SOutput),
+
+    ok.
